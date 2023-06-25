aaa23a0a2e3caca32f1a295f28af2bd76fcb4bee
chiemeliecode <ezeokeke.c@northeastern.edu>
2023-06-25 15:37:02 -0400

    updated xml and xsd

--- src/main/java/com/laba/solvd/databases/Main.java
+++ src/main/java/com/laba/solvd/databases/Main.java
@@ -1,25 +1,9 @@
 package com.laba.solvd.databases;
 
-import com.laba.solvd.databases.model.Album;
-import com.laba.solvd.databases.model.Artists;
-import com.laba.solvd.databases.model.Playlist;
 import com.laba.solvd.databases.model.User;
-import com.laba.solvd.databases.model.UserProfile;
-import com.laba.solvd.databases.model.Wishlist;
 import com.laba.solvd.databases.parser.IParser;
-import com.laba.solvd.databases.parser.JsonParser;
-import com.laba.solvd.databases.parser.XMLParser;
 import com.laba.solvd.databases.parser.musicParser;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.sql.Time;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 import javax.xml.stream.XMLStreamException;
 import org.apache.log4j.Logger;
 
