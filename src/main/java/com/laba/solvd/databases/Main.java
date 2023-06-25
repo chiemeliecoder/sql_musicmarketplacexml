410fa7187a14417c4f431a40eaf4ce4b97d1b169
chiemeliecode <ezeokeke.c@northeastern.edu>
2023-06-25 15:35:41 -0400

    updated xml and xsd

--- src/main/java/com/laba/solvd/databases/Main.java
+++ src/main/java/com/laba/solvd/databases/Main.java
@@ -39,42 +39,9 @@ public class Main {
     System.out.println(users.getPlaylistList().get(0).getPlaylistName());
     System.out.println(users.getWishlistList().get(0).getName());
     System.out.println(users.getWishlistList().get(0).getAlbumList().get(0).getAlbumName());
-//    try {
-//      String jsonFilePath = "src/main/resources/musicdata.json";
-//
-//      User user = JsonParser.parseJsonFile(jsonFilePath);
-//
-//      // Access the parsed user object and perform operations
-//      System.out.println(user.getName());
-////      System.out.println(user.getUserProfile().getBio());
-////      System.out.println(user.getPlaylistList().get(0).getPlaylistName());
-////      System.out.println(user.getWishlistList().get(0).getName());
-////      System.out.println(user.getWishlistList().get(0).getAlbumList().get(0).getAlbumName());
-//    } catch (
-//    IOException e) {
-//      e.printStackTrace();
-//    }
-//
-//
-//    String xmlFile = "src/main/resources/musicdata.xml";
-//    XMLParser xmlParser = new XMLParser();
-//    User user = xmlParser.parseXML(xmlFile);
-//
-//    if (user != null) {
-//      // Handle the parsed user object
-//      System.out.println(user.getName());
-//      System.out.println(user.getUserProfile().getBio());
-//      System.out.println(user.getPlaylistList().get(0).getPlaylistName());
-//      System.out.println(user.getWishlistList().get(0).getName());
-//      System.out.println(user.getWishlistList().get(0).getAlbumList().get(0).getAlbumName());
-//    } else {
-//      // Handle the case when parsing fails
-//      System.out.println("Failed to parse XML.");
-//    }
 
 
-
-    logger.info("end of information");
+    logger.info("end of information for xml parser");
 
 
 
